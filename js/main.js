c6fede49433c3cf1264be9a144675b69416d5b55
Aidar <aidar.lutfullin.1992@gmail.com>
2021-08-11 19:19:47 +0300

    commit 4

--- js/main.js
+++ js/main.js
@@ -4,5 +4,14 @@ $(function () {
       '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow" /></button>',
     prevArrow:
       '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow" /></button>',
+    fade: true,
+    responsive: [
+      {
+        breakpoint: 441,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
 });
